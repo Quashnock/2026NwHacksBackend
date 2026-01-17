fec31a1b171fc51378edd533038a1c8d9fe64a67
Quashnock <awq@uw.edu>
2026-01-17 15:13:48 -0800

    Added test Get route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const port = 3333;
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello!!!!!");
+});
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
